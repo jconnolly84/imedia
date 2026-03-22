e6ada9429cb62927eba14535351e49d5206f8b9c
jconnolly84 <75783119+jconnolly84@users.noreply.github.com>
2026-03-22 10:40:53 +0000

    Delete worksheet directory

--- worksheet/public/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC5SzVKyIzyrroi_HQJ-LJOMQv85fLLVNY",
-  authDomain: "imediagenius.firebaseapp.com",
-  projectId: "imediagenius",
-  storageBucket: "imediagenius.firebasestorage.app",
-  messagingSenderId: "306861941961",
-  appId: "1:306861941961:web:5b8fe140135a652c16d918"
-};
